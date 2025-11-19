f5b8f165196a8405273e86fe6870dae50e946192
XPLJohnPalma <john.palma@xpl.com.au>
2025-11-19 20:48:11 +0800

    Fixed (Just add a placeholder)

---
+++ app/admin/booking-setup/page.tsx
@@ -0,0 +1,11 @@
+
+import React from "react";
+
+export default function BookingSetupPage() {
+  return (
+    <main>
+      <h1>Booking Setup</h1>
+      {/* TODO: build this page later */}
+    </main>
+  );
+}
---
+++ app/auth/create-account/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function CreateAccountPage() {
+  return (
+    <main>
+      <h1>Create Account</h1>
+      {/* TODO: add UI later */}
+    </main>
+  );
+}
