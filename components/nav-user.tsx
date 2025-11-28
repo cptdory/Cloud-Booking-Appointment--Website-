1382d3db9bdab59a9497c117f366c1145d67c715
Francis Lofranco <francisvonjazelofranco@gmail.com>
2025-11-28 22:58:19 +0800

    build-error

--- components/nav-user.tsx
+++ components/nav-user.tsx
@@ -30,7 +30,7 @@ export function NavUser({
     name: string;
     email: string;
     avatar: string;
-    role: "admin" | "customer";
+    role: "admin" | "customer" | "global-admin";
     staffCode?: string;
   };
 }) {
