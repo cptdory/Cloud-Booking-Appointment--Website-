91f57dfe5fc07e266e10bc6e4b1eb8206e3dc4bb
fvjlofranco <francis.lofranco@cloudsteps.com.ph>
2025-12-09 15:40:41 +0800

    build error

--- app/(dashboard)/booking/page.tsx
+++ app/(dashboard)/booking/page.tsx
@@ -1,4 +1,4 @@
-
+import { Suspense } from "react";
 import BookingForm from "@/components/Booking-Form/page";
 
 export default function BookingPage() {
@@ -6,7 +6,9 @@ export default function BookingPage() {
     <div className="flex flex-1 flex-col">
       <div className="@container/main flex flex-1 flex-col gap-2">
         <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
-          <BookingForm />
+          <Suspense fallback={<div>Loading...</div>}>
+            <BookingForm />
+          </Suspense>
         </div>
       </div>
     </div>
