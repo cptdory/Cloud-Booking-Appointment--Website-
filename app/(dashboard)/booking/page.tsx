2787e9ea3345a37cf18391f350c645f917a91e73
fvjlofranco <francis.lofranco@cloudsteps.com.ph>
2025-12-09 15:48:01 +0800

    build bug

--- app/(dashboard)/booking/page.tsx
+++ app/(dashboard)/booking/page.tsx
@@ -1,16 +1,22 @@
 import { Suspense } from "react";
 import BookingForm from "@/components/Booking-Form/page";
 
-export default function BookingPage() {
+function BookingFormWrapper() {
   return (
     <div className="flex flex-1 flex-col">
       <div className="@container/main flex flex-1 flex-col gap-2">
         <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
-          <Suspense fallback={<div>Loading...</div>}>
-            <BookingForm />
-          </Suspense>
+          <BookingForm />
         </div>
       </div>
     </div>
+  );
+}
+
+export default function BookingPage() {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <BookingFormWrapper />
+    </Suspense>
   );
 }
